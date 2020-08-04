76194d64c326b26b9a8a43b31ff857d22a806813
sjory3 <37364942+sjory3@users.noreply.github.com>
2020-08-04 19:06:04 +0200

    made a little bit
    made som small basic stuff

---
+++ Cocktails/Cocktails/Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cocktails
+{
+    class Ingredient
+    {
+        private string Name { get; set; }
+
+        public Ingredient(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Cocktails/Cocktails/Liquid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cocktails
+{
+    class Liquid : Ingredient
+    {
+        private bool Alcohol { get; set; }
+
+        public Liquid(bool alcohol, string name) : base (name)
+        {
+            Alcohol = alcohol;
+        }
+    }
+}
