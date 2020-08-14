275a873c8777ca2fc6a04e5e74700b8ac5c2be6d
Sjory3 <mgj32000@gmail.com>
2020-08-14 14:28:55 +0200

    Update element-service.service.ts
    Got the array from the api and made a new array with the elements obejcts with the data from the api

--- PeriodicSystem/src/app/element-service.service.ts
+++ PeriodicSystem/src/app/element-service.service.ts
@@ -1,34 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Element } from './element';
 import { HttpClient} from '@angular/common/http';
+import { NgForOf } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ElementServiceService {
   //private httpClient: HttpClient
-  constructor(private httpClient: HttpClient) { }
-  
+  constructor(private httpClient: HttpClient) {
+    console.log('Element service called')
+    this.MapData();
+   }
+  request;
   elements:Element[]=[]
 
-
   MapData(): void {
+    console.log('MapData Called')
     this.httpClient.get('https://neelpatel05.pythonanywhere.com/').subscribe(data=>{
-      console.log(data);
+    this.request = data;
+    for (let index = 0; index < 118; index++) {
+      let tempElement = new Element(
+        this.request[index].name,
+        this.request[index].symbol,
+        this.request[index].atomicMass,
+        this.request[index].atomicNumber,
+        this.request[index].standardState
+      )
+      this.elements.push(tempElement);
+      }  
     })
+    console.log(this.elements);
   }
-  }
-/*
-    data.elements.forEach(element => {
-    let tempElement = new Element(
-      element.name,
-      element.symbol,
-      element.atomic_mass,
-      element.number,
-      element.phase
-    );
-    console.log('mapData called');
-    console.log(tempElement.name);
-    this.elements.push(tempElement);
-    });
-  */
+}
