b1c96a9dd8a85dc1ea0c479ad879c852506e697f
Sjory3 <mgj32000@gmail.com>
2020-08-19 12:49:46 +0200

    API call
    made a API call to get the weather and output it to the screen

--- WebAPIAssignment/src/app/app.component.ts
+++ WebAPIAssignment/src/app/app.component.ts
@@ -11,4 +11,18 @@ import { HttpClient} from '@angular/common/http';
 export class AppComponent {
   title = 'WebAPIAssignment';
   
+  ApiKey = '081703f68fc586caeeee2d7f0f008af4';
+  city = 'Ringsted';
+  weather;
+
+  constructor(private httpClient:HttpClient){
+    this.getWeather();
+  }
+
+  getWeather(){
+    this.httpClient.get('http://api.openweathermap.org/data/2.5/weather?q='+this.city+'&units=metric'+'&appid=' + this.ApiKey).subscribe(data=>{
+      this.weather = data;
+      console.log(this.weather);
+    })
+  }
 }
